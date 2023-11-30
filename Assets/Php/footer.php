a561af6a72496f97d193b75075f0f2a610ef5ff8
DovahkiinH <loick.caplier@gmail.com>
2023-11-30 16:35:14 +0100

    PHP

--- Assets/Php/footer.php
+++ Assets/Php/footer.php
@@ -2,9 +2,19 @@
 
 <body>
 
-    <?php 
-    
-    ?>
+    <footer class="" id="footer">
+              <div class="col-12 d-flex justify-content-center">
+
+              </div>
+          </footer>
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+          <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+          <script src="/Assets/Js/Recherche.js"></script>
+      </div>
+  </body>
+</html>
+
     
 </body>
 
--- Assets/Php/header.php
+++ Assets/Php/header.php
@@ -1,11 +1,50 @@
-<html>
 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Keywords" content="Restauration,the district,commande,nourriture">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="/Assets/Css/Css.css">
+    <title>The District</title>
+</head>
 <body>
+<div class="container-fluid">
 
-    <?php 
-    
-    ?>
-    
-</body>
+<header>
 
-</html>
+        <div class="col-12 d-flex" id="menu">
+              <div class="col-1">
+                <img src="/Assets/Img/the_district_brand/logo.png" class="col-8 img-fluid" alt="district_logo">
+              </div>
+              
+                <nav class="navbar navbar-expand-lg">
+
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+
+                  <div class="collapse navbar-collapse" id="navigation">
+
+                    <ul class="navbar-nav">
+            
+                       <li class="nav-item active"> <a class="nav-link fw-bold text-decoration-underline" href="Index.html">Accueil</a> </li>
+            
+                       <li class="nav-item"> <a class="nav-link" href="Categorie.html">Catégorie</a> </li>
+            
+                       <li class="nav-item"> <a class="nav-link" href="Plats.html">Plats</a> </li>
+
+                       <li class="nav-item"> <a class="nav-link" href="Contact.html">Contact</a> </li>
+
+                       <li class="nav-item disabled"> <a class="nav-link" href="Commande.html">Commande</a> </li>
+            
+                    </ul>
+
+                  </div>
+        
+                  </nav>
+
+        </header>
+>
