c03decb0eb3b47a6fba68bb29da95f6f580c00b4
DovahkiinH <loick.caplier@gmail.com>
2023-11-30 16:36:21 +0100

    TheDistrict

--- Assets/Php/footer.php
+++ Assets/Php/footer.php
@@ -2,11 +2,33 @@
 
 <body>
 
-    <footer class="" id="footer">
-              <div class="col-12 d-flex justify-content-center">
+<footer class="" id="footer">
 
-              </div>
-          </footer>
+<div class="col-12 d-flex justify-content-center">
+
+  <a href="Reseau.html">
+    <img src="/Assets/Img/logo/ytb.png" class="col-4 mt-2 rounded-circle zoom" title="Youtube"/>
+    </a>
+
+  <a href="Reseau.html">
+    <img src="/Assets/Img/logo/Fb.jpeg" class="col-4 mt-2 rounded-circle zoom" title="Facebook"/>
+    </a>
+
+  <a href="Reseau.html">
+    <img src="/Assets/Img/logo/Twitter.png" class="col-4 mt-2 rounded-circle zoom" title="Twitter"/>
+    </a>
+
+  <a href="Reseau.html">
+    <img src="/Assets/Img/logo/Insta.jpeg" class="col-4 mt-2 rounded-circle zoom" title="Instagram"/>
+    </a>
+
+  <a href="Reseau.html">
+    <img src="/Assets/Img/logo/Snap.jpg" class="col-4 mt-2 rounded-circle zoom" title="Snapchat"/>
+    </a>
+
+</div>
+
+</footer>
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
           <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
